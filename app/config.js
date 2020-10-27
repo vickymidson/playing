8811a2e4a78d26b9fe8b7c48dc81122c740f313f
“Vicky” <vicky.midson@opencastsoftware.com>
2020-10-27 17:54:35 +0000

    Small changes

--- app/config.js
+++ app/config.js
@@ -4,10 +4,10 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for a pease pudding allowance',
+  serviceName: 'Apply for all the pease pudding',
 
   // Default port that prototype runs on
-  port: '3000',
+  port: '3001',
 
   // Enable or disable password protection on production
   useAuth: 'true',
