05661308adad9202c2dbf4e4aa73e0c5555bdb3c
“Vicky” <vicky.midson@opencastsoftware.com>
2020-10-28 16:38:41 +0000

    Playing with nunjucks

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,38 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.post('/how-many-sandwiches-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var howManySandwiches = req.session.data['how-many-sandwiches']
+
+  // Check whether the variable matches a condition
+  if (howManySandwiches == "Two to nine") {
+    // Send user to next page
+    res.redirect('/pease-pudding-naked')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/ineligible')
+  }
+
+})
+
+router.post('/juggling-balls-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var jugglingBalls = req.session.data['how-many-balls']
+
+  // Check whether the variable matches a condition
+  if (jugglingBalls == "3 or more") {
+    // Send user to next page
+    res.redirect('/juggling-trick')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/ineligible')
+  }
+
+})
 
 module.exports = router
