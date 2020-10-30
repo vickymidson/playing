58c30d29dd6bbde9d48a5260a3ab2d723b05658a
“Vicky” <vicky.midson@opencastsoftware.com>
2020-10-30 11:03:42 +0000

    Trying to get the branching to work

--- app/routes.js
+++ app/routes.js
@@ -18,21 +18,6 @@ router.post('/how-many-sandwiches-answer', function (req, res) {
 
 })
 
-router.post('/juggling-balls-answer', function (req, res) {
 
-  // Make a variable and give it the value from 'how-many-balls'
-  var jugglingBalls = req.session.data['how-many-balls']
-
-  // Check whether the variable matches a condition
-  if (jugglingBalls == "3 or more") {
-    // Send user to next page
-    res.redirect('/juggling-trick')
-  }
-  else {
-    // Send user to ineligible page
-    res.redirect('/ineligible')
-  }
-
-})
 
 module.exports = router
